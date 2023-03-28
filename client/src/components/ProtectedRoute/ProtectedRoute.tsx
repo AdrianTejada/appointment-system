c76d6c476147165b4816f1da059251e81afb41aa
AdrianTejada <atejada@my.bcit.ca>
2023-03-27 20:53:43 -0700

    created public and protected routes

---
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react'
+import { Navigate } from 'react-router-dom'
+
+type ProtectedRouteProps = {
+    children: React.ReactNode
+}
+
+
+
+const ProtectedRoute = ({children}: ProtectedRouteProps): any => {
+    const token = localStorage.getItem('token')
+
+    if (typeof token === 'string') {
+        return children
+    } else {
+        return <Navigate to="/login"/>
+    }
+}
+
+export default ProtectedRoute
