bfd6ce2c0aac602d86f3b6ad38d97010e3d3f04e
AdrianTejada <atejada@my.bcit.ca>
2023-03-30 18:20:02 -0700

    Apply doctor route made

---
+++ client/src/pages/ApplyDoctor/ApplyDoctor.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const ApplyDoctor = () => {
+  return (
+    <Layout>ApplyDoctor</Layout>
+  )
+}
+
+export default ApplyDoctor
